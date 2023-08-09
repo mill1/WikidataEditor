74f226a74f3968eff8a10fd63d1157675c266b82
Emiel Nijhuis <Emiel.Nijhuis@speerit.nl>
2023-08-09 16:45:19 +0200

    Refactored edit comment

--- WikidataEditor/Services/DescriptionService.cs
+++ WikidataEditor/Services/DescriptionService.cs
@@ -16,20 +16,20 @@ namespace WikidataEditor.Services
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task UpsertDescription(string id, string description, string languageCode)
+        public async Task UpsertDescription(string id, string description, string languageCode, string comment)
         {
             var request = new UpdateDescriptionRequestDto
             {
                 description = description,
                 tags = new string[0],
                 bot = false,
-                comment = "Added/updated description"
+                comment = comment
             };
 
             string json = JsonConvert.SerializeObject(request);
             var requestContent = new StringContent(json, Encoding.UTF8, "application/json");
             var httpClient = _httpClientFactory.CreateClient(Constants.HttpClientWikidataRestApi);
-            var uri = $"https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items/{id}/descriptions/{languageCode}";
+            var uri = $"items/{id}/descriptions/{languageCode}";
             var response = await httpClient.PutAsync(uri, requestContent);
             response.EnsureSuccessStatusCode();
         }
