ca9320d95a444c1dbeaa46c500862f57c74cafcf
Emiel Nijhuis <Emiel.Nijhuis@speerit.nl>
2023-08-18 08:59:32 +0200

    Refactored UpsertStatementDoDReference

--- WikidataEditor/Services/StatementService.cs
+++ WikidataEditor/Services/StatementService.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Net;
 using System.Reflection.Emit;
 using System.Xml.Linq;
@@ -52,15 +53,63 @@ namespace WikidataEditor.Services
 
             var statementsDoD = TryGetStatementDoD(statements);
 
-            if(statementsDoD == null) 
+            if (statementsDoD == null)
             {
                 // TODO: Add volledige P570 statement + ref. met meegegeven datum 
-                throw new NotImplementedException("Adding a P570 statement has not been implemented yet.");
-            }            
+                throw new NotImplementedException("Adding a P570 (=DoD) statement has not been implemented yet.");
+            }
 
-            bool existingValueDoD = false;
-            var statementId = string.Empty;
             List<Reference> references = new();
+            var statementId = CheckStatementDoD(dateOfDeath, statementsDoD, ref references);
+
+            if (statementId == string.Empty)
+            {
+                // TODO P570 statement bestaat, maar bestaat nog niet voor de meegegeven datum
+                throw new NotImplementedException("Adding a day to a P570 statement has not been implemented yet.");
+            }
+
+            // Add a new reference to the existing ones
+            references.Add(CreateReference(url));
+
+            var request = CreateUpdateStatementRequest(dateOfDeath, references, statementId);
+
+            string uri = $"items/{id}/statements/{statementId}";
+            _httpClientWikidataApi.PutAsync(uri, request);
+        }
+
+        private static UpdateStatementRequestDto CreateUpdateStatementRequest(DateOnly dateOfDeath, List<Reference> references, string statementId)
+        {
+            return new UpdateStatementRequestDto
+            {
+                statement = new Statement
+                {
+                    id = statementId,
+                    rank = "normal",
+                    property = new Property
+                    {
+                        id = "P570"
+                    },
+                    value = new Value
+                    {
+                        content = new TimeContent
+                        {
+                            time = $"+{dateOfDeath.ToString("yyyy-MM-dd")}T00:00:00Z",
+                            precision = 11,
+                            calendarmodel = "http://www.wikidata.org/entity/Q1985727"
+                        },
+                        type = "value"
+                    },
+                    references = references.ToArray(),
+                },
+                tags = new string[0],
+                bot = false,
+                comment = "Added statement via [[User:Mill 1|Mill 1]]'s edit app using Wikibase REST API 0.1 OAS3"
+            };
+        }
+
+        private string CheckStatementDoD(DateOnly dateOfDeath, JToken? statementsDoD, ref List<Reference> references)
+        {
+            var statementId = string.Empty;
 
             foreach (var child in statementsDoD)
             {
@@ -69,31 +118,34 @@ namespace WikidataEditor.Services
                 if (time == null)
                     continue;
 
-                var dateString = ((string)time).Substring(1,10);
+                var dateString = ((string)time).Substring(1, 10);
                 DateOnly date;
                 if (DateOnly.TryParse(dateString, out date))
                 {
                     if (date == dateOfDeath)
                     {
-                        existingValueDoD = true;
                         statementId = child["id"].ToString();
                         references = child["references"].ToObject<Reference[]>().ToList();
 
                         if (!references.Any())
                             break;
-                       
-                        // TODO: dit moet beter kunnen
-                        var part = references.Where(r => r.parts.Any(p => p.property.id == "P248")) // "P248") =  'stated in'
-                            .Select(r => r.parts.Where(p => p.property.id == "P248")).FirstOrDefault();
 
-                        if (part != null)
-                            if (part.First().value.content.ToString() == "Q11148") // "Q11148" = 'The Guardian'
-                                throw new HttpRequestException("Reference already exists", null, HttpStatusCode.BadRequest);
+                        var existingReference = references.SelectMany(r => r.parts, (reference, part) => new { reference, part })
+                                    .Where(refAndPart => refAndPart.part.property.id == "P248") // "P248" = 'stated in'
+                                    .Where(x => x.part.value.content.ToString() == "Q11148");   // "Q11148" = 'The Guardian'
+
+                        if (existingReference.Any())
+                            throw new HttpRequestException("Reference already exists", null, HttpStatusCode.BadRequest);
                     }
-                }            
+                }
             }
 
-            references.Add(new Reference
+            return statementId;
+        }
+
+        private Reference CreateReference(string url)
+        {
+            return new Reference
             {
                 parts = new Part[]
                 {
@@ -142,62 +194,8 @@ namespace WikidataEditor.Services
                         }
                     }
                 }
-            });
-
-            // TODO: if not present also add ref to English wikipedia (= Q328). Plus retrieved?
-            /*
-            references.Add(new Reference
-            {
-                parts = new Part[]
-                {
-                    new Part
-                    {
-                        property = new Property
-                        {
-                            id = "P143",
-                            datatype = "wikibase-item"
-                        },
-                        value = new Value
-                        {
-                            type = "value",
-                            content = "Q328"
-                        }
-                    }
-                }
-            });
-            */
-
-            var request = new UpdateStatementRequestDto
-            {
-                statement = new Statement
-                {
-                    id = statementId,
-                    rank = "normal",
-                    property = new Property 
-                    { 
-                        id = "P570" 
-                    },
-                    value = new Value
-                    {
-                        content = new TimeContent
-                        {
-                            time = $"+{dateOfDeath.ToString("yyyy-MM-dd")}T00:00:00Z",
-                            precision = 11,
-                            calendarmodel = "http://www.wikidata.org/entity/Q1985727"
-                        },
-                        type = "value"
-                    },
-                    references = references.ToArray(),
-                },
-                tags = new string[0],
-                bot = false,
-                comment = "Added statement via [[User:Mill 1|Mill 1]]'s edit app using Wikibase REST API  0.1  OAS3"
             };
-
-            string uri = $"items/{id}/statements/{statementId}";
-            _httpClientWikidataApi.PutAsync(uri, request);
         }
-
  
         private JToken? TryGetStatementDoD(JObject statements)
         {
